fc49087d92c326c19df9f512149785dec41b543e
MacBook Air M1 <macbookairm1@MacBook-Air-de-Felipe-Colenghi.local>
2023-07-17 15:30:14 -0300

    modify

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 img = "";
+status2 = "";
+objects = []; 
 
 function setup()
 {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status2 = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results){
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 function preload()
 {
@@ -17,4 +33,18 @@ function draw()
     noFill();
     stroke("#FF0000");
     rect(30, 60, 450, 350);
+    
+    if (status2 != "") {
+        fill("#FF0000");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(30, 60, 450, 350);
+
+    fill("#FF0000");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 370, 320);
+    }
 }
